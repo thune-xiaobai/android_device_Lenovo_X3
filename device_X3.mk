e63fe118874c5258b478c6a964773d9e8451a837
xiaobai <fengz181x@126.com>
2016-06-16 00:09:48 +0800

    X3:add keylayout

--- device_X3.mk
+++ device_X3.mk
@@ -68,11 +68,6 @@ PRODUCT_COPY_FILES += \
 
 #keylayout
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/ft5x46.kl:system/usr/keylayout/ft5x46.kl \
-    $(LOCAL_PATH)/Generic.kl:system/usr/keylayout/Generic.kl \
-    $(LOCAL_PATH)/atmel-maxtouch.kl:system/usr/keylayout/atmel-maxtouch.kl \
-    $(LOCAL_PATH)/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
-    $(LOCAL_PATH)/synaptics_dsx_edge.kl:system/usr/keylayout/synaptics_dsx_edge.kl \
     $(LOCAL_PATH)/synaptics_dsx.kl:system/usr/keylayout/synaptics_dsx.kl
 
 #hostapd
